1af0045c14924e0e9d9b3de5abc1f5a138689ce7
savger1024 <savger1024@gmail.com>
2024-01-09 20:46:00 +0100

    Day4, arrays

--- Day3_240108.cpp
+++ Day3_240108.cpp
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void Day4Task1() {
+    int n;
+    std::cin >> n;
+    cout << n << '\n';
+    int A[n];
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        A[i] = temp;
+    }
+    
+    for (int i = n-1; i >= 0; i--) {
+        cout << A[i];
+    }
+}
 
 void update(int *a,int *b) {
     int temp = *a;
@@ -8,6 +30,11 @@ void update(int *a,int *b) {
 }
 
 int main() {
+    cout << "hi";
+
+    //24.01.09.
+    Day4Task1();
+
     int a, b;
     int *pa = &a, *pb = &b;
     
